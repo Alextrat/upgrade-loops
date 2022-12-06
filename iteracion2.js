72e3b05efa2cd93eba7c291a76776188dab5aee5
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2022-12-06 18:29:50 +0100

    subo los 5 primeros iteración#1, #2,#3,#4 y #5

---
+++ iteracion2.js
@@ -0,0 +1,40 @@
+'use strict'
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+
+for(let i =0; i <alumns.length; i++){
+    if(alumns[i].T1 + alumns[i].T2 + alumns[i].T3 >=2 ){
+        console.log('el alumno '+ alumns[i].name + ' está aprobado')
+        alumns[i].isAppoved = true
+    }else{
+        console.log('el alumno '+ alumns[i].name + ' está suspenso')
+        alumns[i].isAppoved = false
+    }
+
+}
+console.log(alumns)
+
+// for(let i=0 ; i <alumns.length; i++){
+//     if(alumns[i].T1 && alumns[i].T2){
+//         console.log('el alumno '+ i + 'está aprobado')  
+//         // alumns[i].name +(isAppoved='true') 
+//     }else if(alumns[i].T1 && alumns[i].T3){   
+//         console.log('el alumno '+ i + 'está aprobado') 
+//         // alumns[i].push(isAppoved='true') 
+//     }else if(alumns[i].T2 && alumns[i].T3){
+//         console.log('el alumno '+ i + 'está aprobado') 
+//         // alumns[i].push(isAppoved='true') 
+//     }else{
+//         console.log('el alumno '+ i + 'está suspenso') 
+//         // alumns[i].push(isAppoved='false')
+//     }
+// }
+// console.log(alumns)
+
+// alumns[i].isAppoved 
---
+++ iteracion3.js
@@ -0,0 +1,5 @@
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for(recorrer of placesToTravel){
+    console.log(recorrer)
+}
