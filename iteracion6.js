2a1b463482f605e5e5f2086a474e7e950f30e444
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2022-12-08 18:23:40 +0100

    los siete ejercicios funcionando bien

--- iteracion6.js
+++ iteracion6.js
@@ -1,17 +1,30 @@
-const toys = [
-    {id: 5, name: 'Buzz MyYear'}, 
-    {id: 11, name: 'Action Woman'}, 
-    {id: 23, name: 'Barbie Man'}, 
-    {id: 40, name: 'El gato con Guantes'},
-    {id: 40, name: 'El gato felix'}
-    ]
-
-    for(let posicion of toys){
-        if (posicion.name.includes("gato")== true){
-  
-            toys.splice(posicion,1) 
-            // console.log(toys)
-            
+     const toys = [
+        {id: 5, name: 'Buzz MyYear'}, 
+        {id: 11, name: 'Action Woman'}, 
+        {id: 23, name: 'Barbie Man'}, 
+        {id: 40, name: 'El gato con Guantes'},
+        {id: 40, name: 'El gato felix'}
+        ]
+    
+        for(let i = toys.length-1 ; i>=0; i--){
+            if (toys[i].name.includes("gato")== true){
+      
+                toys.splice(i,1)         
+            }
         }
-    }
-    console.log(toys)
+        console.log(toys)
+    
+
+    // for(let posicion of toys){
+    //     if (posicion.name.includes("gato")== true){
+  
+    //         toys.splice(posicion,1) 
+            
+            
+    //     }
+    // }
+    // console.log(toys)
+
+
+    // //hacerlo con un for tradicional alrevés
+    
